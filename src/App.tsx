3b302c879b8c6cd8b908660839f4cb0caceb5e67
André Navarro <andre.navarro@bistwobis.com.br>
2022-06-26 13:58:31 -0300

    :sparkles: Added Sidebar component - Ignite Lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
+import { Event } from "./pages/Event"
 
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
 
 function App() {
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-
-  return (
-    <ul>
-      {data?.lessons.map(lessons => {
-        return <li key={lessons.id}>{lessons.title}</li>
-      })}
-    </ul>
-  )
+    return (
+        <div>
+            <Event />
+        </div>
+    )
 }
 
 export default App
